b734b87c9c10af810e7fbf53965fc581732cd0bc
Calvin Brown <calvin.brown@banno.com>
2018-07-31 07:19:38 -0500

    First commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Calvin Brown on 7/31/18.
+//  Copyright © 2018 Calvin Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
